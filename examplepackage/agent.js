f2fda151b9fba4cb70f5440668ce34259cd98ea8
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2016-06-10 14:02:55 +0300

    camera focus and crawling and some other UI related things are done

--- examplepackage/agent.js
+++ examplepackage/agent.js
@@ -1,76 +1,77 @@
-
-function Agent() {
-  this.work = "";
-  this.preStopWork = "";
-  this.preStartWork = "";
-  this.timer = true;
-  this.repeat = true;
-  this.interval = 1000;
-}
-
-Agent.prototype.s = function() {
-    this.createInterval(this.work, function(restartMainMessage){
-        if(restartMainMessage){
-            console.log(restartMainMessage);
-        }
-        if(this.timer) {
-            this.s();
-        } else {
-            this.preStopWork(function(stopExecutionMessage){
+function Agent(repeat, interval){
+    
+    var that = {};
+    
+    // public functions that will be overriden by applications
+    that.main;
+    that.initialize;
+    that.terminate;
+    
+    // private property
+    var timer = true;
+    
+    // public properties
+    that.repeat = repeat || true;
+    that.interval = interval || 1000;
+    
+    that.configureInterval = function(repeat, interval) {
+        that.repeat = repeat;
+        that.interval = interval;
+    }
+    
+    var createInterval = function(f, param, interval) {
+        setTimeout( function() {f(param);}, interval );
+    }
+    
+    var s = function() {
+        createInterval(that.main, function(restartMainMessage){
+            if(restartMainMessage){
+                console.log(restartMainMessage);
+            }
+            if(timer) {
+                s();
+            } else {
+                that.terminate(function(stopExecutionMessage){
+                    if(stopExecutionMessage){
+                        console.log(stopExecutionMessage);
+                    }
+                });
+            }
+        }, that.interval);
+    };
+    
+    that.start = function() {
+      that.initialize(function(startMainMessage){
+          if(startMainMessage){
+              console.log(startMainMessage);
+          }
+              timer = true;
+              that.main(function(restartMainMessage){
+                  if(restartMainMessage){
+                      console.log(restartMainMessage);
+                  }
+                  console.log("app-started-without-error");
+                  if(that.repeat) {
+                      s();
+                  }
+              });
+      });
+    };
+    
+    that.stop = function() {
+        timer = false;
+        if(!that.repeat) {
+            that.terminate(function(stopExecutionMessage){
                 if(stopExecutionMessage){
                     console.log(stopExecutionMessage);
                 }
             });
         }
-    }.bind(this), this.interval);
-}
-
-Agent.prototype.createInterval = function(f, param, interval) {
-    setTimeout( function() {f(param);}, interval );
-}
-
-Agent.prototype.setWork = function(mainFunc, repeat, interval) {
-  this.work = mainFunc;
-  this.repeat = repeat;
-  this.interval = interval;
-}
-
-Agent.prototype.start = function() {
-  this.preStartWork(function(startMainMessage){
-      if(startMainMessage){
-          console.log(startMainMessage);
-      }
-          this.timer = true;
-          this.work(function(restartMainMessage){
-              if(restartMainMessage){
-                  console.log(restartMainMessage);
-              }
-              console.log("app-started-without-error");
-              if(this.repeat) {
-                  this.s();
-              }
-          }.bind(this));
-      
-  }.bind(this));
-}
-
-Agent.prototype.setPreStopWork = function(preStopFunc) {
-  this.preStopWork = preStopFunc;
-}
-
-Agent.prototype.setPreStartWork = function(preStartFunc) {
-  this.preStartWork = preStartFunc;
-}
-
-Agent.prototype.stop = function() {
-    this.timer = false;
-    if(!this.repeat) {
-        this.preStopWork(function(stopExecutionMessage){
-            if(stopExecutionMessage){
-                console.log(stopExecutionMessage);
-            }
-        });
-    }
+    };
+        
+    return that;
 }
 
 module.exports = Agent;
+
+
