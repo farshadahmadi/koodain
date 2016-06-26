8ade8c0d9582aee8d7e589f9794543aa0b2440c6
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2016-06-26 23:26:43 +0300

    change visual devices file to examplepackage folder

--- server/api/visualdevice/visualdevice.controller.js
+++ server/api/visualdevice/visualdevice.controller.js
@@ -8,7 +8,8 @@ var fs = require("fs");
 
 // Gets a list of Visualdevices
 exports.index = function (req, res) {
-  var visualDevs = fs.readFileSync(__dirname + "/../../../visualdevices/visualdevices.json");
+  //var visualDevs = fs.readFileSync(__dirname + "/../../../visualdevices/visualdevices.json");
+  var visualDevs = fs.readFileSync("./examplepackage/visualdevices.json");
   //res.json([]);
   res.status(200).send(visualDevs);
 }
